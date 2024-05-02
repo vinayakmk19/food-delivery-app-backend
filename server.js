7206b506e2eea48eaaf18daa9889fbaf3d23f704
vinayakmk19 <vinayakmkgen@gmail.com>
2024-05-02 18:39:12 +0530

    created backend seetup

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+
+
+const app = express();
+const port = 4000;
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+        res.send("API Working!!");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is Started on http://localhost:${port}`)
+})
